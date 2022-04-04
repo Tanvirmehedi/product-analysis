aa8ae158480ec17601f9310977dea4bb9e1f29c0
Tanvirmehedi <tjmehedi201@gmail.com>
2022-04-05 00:00:20 +0600

    Customer Section Functionality Added

--- src/components/Reviews/Review.js
+++ src/components/Reviews/Review.js
@@ -5,7 +5,7 @@ import DisplayReview from "./DisplayReview";
 const Review = () => {
   const [reviews, setReviews] = useReview();
   return (
-    <div className="md:container mx-auto">
+    <div className="md:container mx-auto mt-3">
       <h1>Total Data Found: {reviews.length}</h1>
       <div className="grid md:grid-cols-3 gap-4 content-center mt-3">
         {reviews.map((review) => (
--- src/contents/CustomerReview/CustomerReview.js
+++ src/contents/CustomerReview/CustomerReview.js
@@ -1,14 +1,30 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import useReview from "../../hooks/useReview";
+import SortReview from "./SortReview";
 
 const CustomerReview = () => {
-  const navigate = useNavigate();
-  return (
-    <div className="my-3">
-      <h1>customer review</h1>
+  const navigate = useNavigate([]);
 
+  const [reviews, setreviews] = useReview();
+
+  const result = reviews.filter((elem, index) => index < 3);
+  return (
+    <div className="my-10 md:container mx-auto">
+      <h1 className="text-2xl font-semibold bg-slate-100 inline-block px-3 py-1 rounded-md my-3">
+        customer review ({result ? result.length : "Data Not Found"})
+      </h1>
+      {result ? (
+        <div className="grid md:grid-cols-3 gap-3 md:my-6">
+          {result.map((reviewer) => (
+            <SortReview key={reviewer.id} reviewer={reviewer}></SortReview>
+          ))}
+        </div>
+      ) : (
+        "Data Not Found"
+      )}
       <button
-        className="bg-slate-400 px-3 py-1 rounded-md font-bold Font__Lobster"
+        className="bg-slate-400 px-3 py-1 rounded-md font-bold Font__Lobster mt-6"
         onClick={() => navigate("/reviews")}
       >
         See All Review
