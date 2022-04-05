f31188a0892b989b5feed890d40c86005fac719a
Tanvirmehedi <tjmehedi201@gmail.com>
2022-04-05 11:55:15 +0600

    blog page design Complite

--- src/components/Blogs/Blog.js
+++ src/components/Blogs/Blog.js
@@ -1,9 +1,35 @@
 import React from "react";
 
 const Blog = () => {
+  const questionAnswer = [
+    {
+      question: "What Is Context Api?",
+      answer: "Answer",
+    },
+    {
+      question: "what is semantic tag?",
+      answer: "Answer",
+    },
+    {
+      question: "Difference between inline block and  block element.",
+      answer: "Answer",
+    },
+  ];
   return (
-    <div>
-      <h1>Blog</h1>
+    <div className="grid md:grid-cols-3 md:container mx-auto gap-5 my-3 ">
+      {questionAnswer.map((data) => (
+        <div className="border rounded-md py-2 px-1 mx-3">
+          <h3 className="bg-slate-500 text-slate-200 rounded-sm text-xl">
+            {data.question}
+          </h3>
+          <p>
+            <span className="text-orange-400 uppercase font-semibold">
+              Answer:{" "}
+            </span>
+            {data.answer}
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
