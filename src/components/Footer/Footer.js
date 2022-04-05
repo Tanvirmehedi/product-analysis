2061d640b5746aba52bf71d617f38fa92673255f
Tanvirmehedi <tjmehedi201@gmail.com>
2022-04-05 12:57:57 +0600

    Footer Edit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <footer className="bg-slate-400 py-4 ">
         <p>
           All Right Reserved &copy;
-          <span className="text-slate-300 hover:font-bold">
+          <span className="text-slate-300 transition-all font-semibold Font__Lobster hover:font-bold">
             <Link to="/"> The Alchemist</Link> 2022
           </span>
         </p>
